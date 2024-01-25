095ca8ee11608fa2853c20ddcaa0961918941d5d
bobbychansfu <bobbychansfu@gmail.com>
2024-01-25 09:56:08 -0800

    JavaScript 1: inputs, selectors, addEventListener

---
+++ demo/src/main/resources/static/javascript/script.js
@@ -0,0 +1,76 @@
+// n = window.prompt("Enter a name:");
+// window.alert("hello " + n)
+
+// if (true) {
+//     console.log("hello")
+// }
+
+// let x = 5
+
+// arrays 
+
+/*
+let arr = [1, 2, 3, 4, 5]
+arr = arr + [6, 7, 8]
+
+arr.push(9)
+let x = arr.pop()
+
+arr.unshift(0)
+let y = arr.shift()
+*/
+
+// objects - JSON
+
+let obj = {
+    name: "John",
+    age: 25,
+    address: {
+        city: "Bangalore",
+        state: "Karnataka"
+    },
+    hobbies: ["reading", "coding", "music"],
+    sayHello: function () {
+        console.log("Hello")
+    }
+}
+
+let obj2 = {
+    name: "Mary",
+    age: 25,
+    address: {
+        city: "Bangalore",
+        state: "Karnataka"
+    },
+    hobbies: ["reading", "coding", "music"]
+}
+
+arr = [obj, obj2]
+
+arr[0].sayHello()
+
+// function
+
+// function sayHello() {
+//     console.log("Hello")
+// }
+
+let operate = function () {
+    console.log("Hello")
+}
+
+// operate()  // call function
+
+// window.setInterval(operate, 500)  // call function every 1/2 second
+
+// let a = document.querySelector("h1")
+// a.innerHTML = "Hello"
+
+
+
+// perform math operation
+
+var num1 = document.querySelector("#num1").value
+var num2 = document.querySelector("#num2").value
+
+document.querySelector("form").addEventListener("submit", operate)
